fe2f5acbed2481f49532715dd0b4540abeebe689
aloosli <lizzwizz@tuta.io>
2023-01-21 11:34:04 +0000

    Changed color scheme, added delete button, added logic for delete button to delete data from local storage

--- scheduler.js
+++ scheduler.js
@@ -5,7 +5,6 @@ const container = $(".container");
 for (let i = 8; i <= 17; i++) {
   // create a new row for each hour
   const row = $("<div>").addClass("row time-block ");
-  
 
   // create a div for the hour
   const hour = $("<div>").addClass("hour col-2").text(`${i}:00`);
@@ -14,15 +13,20 @@ for (let i = 8; i <= 17; i++) {
 
   // create a button for saving the event
   const saveBtn = $("<button>")
-    .addClass("saveBtn col-2")
+    .addClass("saveBtn col-1")
     .html('<i class="fas fa-lg fa-save"></i>');
+   // create a button for deleting the event
+  const deleteBtn = $("<button>")
+    .addClass("deleteBtn col-1")
+    .html('<i class="fas fa-lg fa-trash"></i>');
 
-  // append the hour, event, and saveBtn to the row
-  row.append(hour, event, saveBtn);
+
+  // append the hour, event,saveBtn, and delete button to the row
+ 
+  row.append(hour, event, saveBtn,deleteBtn );
 
   // append the row to the container div
   container.append(row);
-  
 }
 
 // select all the timeblock rows
@@ -31,13 +35,12 @@ const rows = $(".row");
 // get the current hour
 const currentHour = moment().hour();
 
-
+// color the rows based on the current hour
+//-------------------------------------------------------------------------
 // loop through the rows
 rows.each(function () {
   // get the hour of the current row
-  const rowHour = parseInt($(this).find(".hour").text().substr(0,2));
-
-
+  const rowHour = parseInt($(this).find(".hour").text().substr(0, 2));
 
   // compare the hour of the current row to the current hour
   if (rowHour < currentHour) {
@@ -52,4 +55,49 @@ rows.each(function () {
   }
 });
 
+// Save the event to local storage
+//-----------------------------------------------------------------------------
 
+// add event listener to the save button
+$(".saveBtn").on("click", function () {
+  // get the textarea value
+  const event = $(this).siblings(".description").val();
+  // get the hour of the row
+  const hour = $(this).siblings(".hour").text();
+
+  // save the event to local storage
+  localStorage.setItem(hour, event);
+});
+
+// load the events from local storage
+//-----------------------------------------------------------------------------
+
+function loadEvents() {
+  // loop through the rows
+  rows.each(function () {
+    // get the hour of the current row
+    const hour = $(this).find(".hour").text();
+    // get the event from local storage
+    const event = localStorage.getItem(hour);
+
+    // if there is an event, set the textarea value to the event
+    if (event) {
+      $(this).find(".description").val(event);
+    }
+  });
+
+}
+// delete the event from local storage
+$('.deleteBtn').on("click", function() {
+  // get the hour of the row
+  const hour = $(this).siblings(".hour").text();
+  // remove the event from local storage
+  localStorage.removeItem(hour);
+  // clear the textarea
+  $(this).siblings(".description").val("");
+});
+
+  // call the loadEvents function when the page loads
+  $(document).ready(function () {
+    loadEvents();
+  });
